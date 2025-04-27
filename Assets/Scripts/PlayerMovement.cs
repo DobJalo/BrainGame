1166b3b74bd78fb5bc57e10c09b0b5e11b2c752a
DobJalo <marugrig@gmail.com>
2025-04-27 10:21:29 +0300

    Settings_sensivity
    Mouse sensivity has been added in settings

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 0.2f;
-    public float lookSpeed = 2f;
+    public float lookSpeedX = 2f;
+    public float lookSpeedY = 2f;
 
-    public Transform cameraTransform; 
+    public Transform cameraTransform;
     private Rigidbody rb;
     private float xRotation = 0f;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); //Get Rigidbody component from Player
+        rb = GetComponent<Rigidbody>(); // Get Rigidbody component from Player
+
+        // Mouse Sensitivity
+        if (PlayerPrefs.HasKey("MouseSensitivity"))
+        {
+            float savedSensitivity = PlayerPrefs.GetFloat("MouseSensitivity");
+            lookSpeedX = savedSensitivity;
+            lookSpeedY = savedSensitivity;
+        }
     }
 
-
-    void FixedUpdate() //FixedUpdate is better for synchronization physics and movement
+    void FixedUpdate() // FixedUpdate is better for synchronization physics and movement
     {
         // Player movement
         float moveX = Input.GetAxis("Horizontal");
@@ -25,12 +33,11 @@ public class PlayerMovement : MonoBehaviour
         rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
     }
 
-        void Update()
-        {
-
-            // Get mouse movement
-            float mouseX = Input.GetAxis("Mouse X") * lookSpeed;
-        float mouseY = Input.GetAxis("Mouse Y") * lookSpeed;
+    void Update()
+    {
+        // Get mouse movement
+        float mouseX = Input.GetAxis("Mouse X") * lookSpeedX;
+        float mouseY = Input.GetAxis("Mouse Y") * lookSpeedY;
 
         // Rotate player left/right
         transform.Rotate(Vector3.up * mouseX);
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.LowLevel;
 
 public class Settings : MonoBehaviour
 {
+    //volume
     public Slider volumeSlider;
     public AudioSource audioSource;
-
     private const string VolumeKey = "Volume"; //save slot
+
+    //mouse sensivity 
+    public Slider sensitivitySlider;
+    private const string SensitivityKey = "MouseSensitivity"; //save slot
+    private int sensivityInteger = 7;
 
     void Start()
     {
+        //volume
         if (PlayerPrefs.HasKey(VolumeKey))
         {
             float savedVolume = PlayerPrefs.GetFloat(VolumeKey);
@@ -21,8 +28,20 @@ public class Settings : MonoBehaviour
         {
             volumeSlider.value = audioSource.volume;
         }
-
         volumeSlider.onValueChanged.AddListener(ChangeVolume);
+
+
+        //mouse sensivity
+        if (PlayerPrefs.HasKey(SensitivityKey))
+        {
+            float savedSensitivity = PlayerPrefs.GetFloat(SensitivityKey);
+            sensitivitySlider.value = savedSensitivity/sensivityInteger;
+        }
+        else
+        {
+            sensitivitySlider.value = 2f; 
+        }
+        sensitivitySlider.onValueChanged.AddListener(ChangeSensitivity);
     }
 
     void ChangeVolume(float value)
@@ -33,10 +52,20 @@ public class Settings : MonoBehaviour
         PlayerPrefs.Save();
     }
 
-
+    void ChangeSensitivity(float value)
+    {
+        PlayerPrefs.SetFloat(SensitivityKey, value*sensivityInteger);
+        PlayerPrefs.Save();
+    }
 
     public void Back()
     {
         SceneManager.LoadScene("Main Menu");
     }
+
+
+
+
+
+ 
 }
